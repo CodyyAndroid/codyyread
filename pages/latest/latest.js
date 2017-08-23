c8c55879ce0f69e82e506cd8ce10c9fb3297982e
jdpxiaoming <caixingming@codyy.com>
2017-08-23 17:40:45 +0800

    扫一扫

--- pages/latest/latest.js
+++ pages/latest/latest.js
@@ -6,9 +6,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    title:'最新消息',
-    latest:[],
-    hidden:false
+    title: '最新消息',
+    latest: [],
+    hidden: false
   },
   /**
    * 生命周期函数--监听页面加载
@@ -16,12 +16,12 @@ Page({
   onLoad: function (options) {
     this.fetchData();
   },
-  onPullDownRefresh: function(){
+  onPullDownRefresh: function () {
     this.fetchData();
-    console.log("onPullDownRefresh",new Date());
+    console.log("onPullDownRefresh", new Date());
   },
-  redictDetail: function(e){
-    var id = e.currentTarget.id,url = '../detail/detail?id='+id;
+  redictDetail: function (e) {
+    var id = e.currentTarget.id, url = '../detail/detail?id=' + id;
 
     wx.navigateTo({
       url: url,
@@ -56,14 +56,46 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
-  },
-  scan:function(){
-    wx.scanCode({
-      success:(res)=>{
-        console.log(res);
-      }
 
+  },
+  scan: function () {
+    wx.scanCode({
+      success: (res) => {
+        console.log(res);
+        var isbn = res.result;
+        //查询数据
+        var url = Api.getBookInfo(isbn);
+        var that = this;
+        wx.request({
+          url: url,
+          header: {
+            'content-type': 'application/x-www-form-urlencoded'
+          },
+          success: function (res) {
+            console.log(res);
+
+            //跳转到扫一扫详情.
+            that.goScanDetail({
+              currentTarget: {
+                id: '',
+                title: res.data.alt_title
+              }
+            })
+
+          }
+        })
+      }
+    })
+  },
+  goScanDetail: function (e) {
+
+    var id = e.currentTarget.id;
+    var title = e.currentTarget.title;
+    var url = '../scan/scan?id=' + id + '&title=' + title;
+    console.log(url);
+    wx.navigateTo({
+      url: url,
     })
   }
+  ,
 })
--- pages/register/register.js
+++ pages/register/register.js
@@ -134,6 +134,6 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+   
   }
 })
